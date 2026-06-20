eeaee872a6abe2be165a3be39077c073f6a127f5
vedikagrawal <agrawalvedika95@gmail.com>
2026-06-20 21:43:59 +0530

    added for goggle analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -98,6 +98,20 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
     ],
+    scripts: [
+      {
+        src: "https://www.googletagmanager.com/gtag/js?id=G-5BXGSYK45Z",
+        async: true,
+      },
+      {
+        children: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5BXGSYK45Z');
+        `,
+      },
+    ],
   }),
   shellComponent: RootShell,
   component: RootComponent,
